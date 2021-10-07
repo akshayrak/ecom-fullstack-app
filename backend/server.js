629e33c8bab27eb74c6074bf85c2b36079245218
Akshay R <akshayrak77@gmail.com>
2021-10-08 01:09:18 +0530

    created folders inside backend and setup of mongoDb is done, data is imported

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
 const express = require('express')
+const connectDB = require('./config/db.js')
+connectDB()
 const app=express()
 
-const {PORT} = process.env||5000
+const PORT = process.env.PORT||5000
 
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`))
